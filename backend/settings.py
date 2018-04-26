cba59f64a9ba219faffbb581140be2d40cef9ca4
Jens Nistler <jens.nistler@regiohelden.de>
2018-04-26 13:25:44 +0200

    enable logging

--- backend/settings.py
+++ backend/settings.py
@@ -81,6 +81,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
